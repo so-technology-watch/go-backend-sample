d95afed9b1daf8a9cafa13a2f5e0967b9c08757f
Xavier MARC <xavmarc@gmail.com>
2017-02-09 17:45:24 +0100

    :wrench: config file

--- config/initRedisDB.go
+++ config/initRedisDB.go
@@ -1,25 +1,41 @@
 package config
 
 import (
+	"encoding/json"
+	"os"
 	"fmt"
 	"gopkg.in/redis.v5"
 )
 
+type DBConfiguration struct {
+	Url    string
+	Port   string
+	Password string
+	Db int
+}
+
 var DB *redis.Client
 
 func init() {
-	// TODO externaliser config
+	var dbConfiguration DBConfiguration
+	file, _ := os.Open("dbConfig.json")
+	decoder := json.NewDecoder(file)
+	err := decoder.Decode(&dbConfiguration)
+	if err != nil {
+		fmt.Println("File config error :", err)
+	}
+
 	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
+		Addr:     dbConfiguration.Url + ":" + dbConfiguration.Port,
+		Password: dbConfiguration.Password,
+		DB:       dbConfiguration.Db,
 	})
 
 	pong, err := client.Ping().Result()
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println("Connexion DB error :", err.Error())
 	} else {
-		fmt.Println("Connexion BDD OK : " + pong)
+		fmt.Println("Connexion DB OK :", pong)
 	}
 	DB = client
 }
