2eff9b965c09dd3a5160695ed91ee08078717716
Xavier MARC <xavmarc@gmail.com>
2018-01-08 22:10:23 +0100

    step 7

--- todolist.go
+++ todolist.go
@@ -10,20 +10,19 @@ import (
 )
 
 var (
-	taskDAO        dao.TaskDAO
-	taskController *web.TaskController
-
-	port, db int
-	dbFile, logLevel string
+	port               = 8020
+	logLevel           = "warning"
+	db                 = 1
+	dbConfigFile       = ""
 )
 
 // Main
 func main() {
 	// Get arguments
-	flag.IntVar(&port, "p", 8020, "webserver port")
-	flag.IntVar(&db, "db", 1, "database (0 - Redis | 1 - Mock)")
-	flag.StringVar(&dbFile, "dbFile", "", "config file path")
-	flag.StringVar(&logLevel, "log", "debug", "log level")
+	flag.IntVar(&port, "p", port, "webserver port")
+	flag.IntVar(&db, "db", db, "database (0 - Redis | 1 - Mock)")
+	flag.StringVar(&dbConfigFile, "dbConf", dbConfigFile, "config file path")
+	flag.StringVar(&logLevel, "log", logLevel, "log level")
 
 	// Parse arguments
 	flag.Parse()
@@ -34,35 +33,12 @@ func main() {
 		fmt.Println(err)
 	}
 
-	// New Controller
-	taskController = web.NewTaskController(taskDAO)
+	// New controller
+	taskController := web.NewTaskController(taskDAO)
 
-	http.HandleFunc("/", welcomeHandler)
-
-	// Tasks Handler
-	http.HandleFunc("/tasks", tasksHandler)
-
+	// New router
+	router := web.NewRouter(taskController)
+	
 	fmt.Println("Starting web server on port : " + strconv.Itoa(port))
-	http.ListenAndServe(":"+strconv.Itoa(port), nil)
-}
-
-// Welcome handler
-func welcomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome !")
-}
-
-// Tasks handler
-func tasksHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		taskController.GetTask(w, r)
-	case "PUT":
-		taskController.UpdateTask(w, r)
-	case "POST":
-		taskController.CreateTask(w, r)
-	case "DELETE":
-		taskController.DeleteTask(w, r)
-	default:
-		fmt.Fprintf(w, "Sorry only GET, POST, PUT and DELETE methods are supported.")
-	}
+	http.ListenAndServe(":"+strconv.Itoa(port), router)
 }
