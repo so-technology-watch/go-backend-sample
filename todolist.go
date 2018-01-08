41cc0289315a6daa93e55d28f9652c4123a66d87
Xavier MARC <xavmarc@gmail.com>
2018-01-08 22:18:33 +0100

    step 8

--- todolist.go
+++ todolist.go
@@ -3,27 +3,26 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net/http"
 	"go-backend-sample/dao"
 	"go-backend-sample/web"
 	"strconv"
+	"github.com/urfave/negroni"
 )
 
 var (
-	taskDAO        dao.TaskDAO
-	taskController *web.TaskController
-
-	port, db int
-	dbFile, logLevel string
+	port               = 8020
+	logLevel           = "warning"
+	db                 = 1
+	dbConfigFile       = ""
 )
 
 // Main
 func main() {
 	// Get arguments
-	flag.IntVar(&port, "p", 8020, "webserver port")
-	flag.IntVar(&db, "db", 1, "database (0 - Redis | 1 - Mock)")
-	flag.StringVar(&dbFile, "dbFile", "", "config file path")
-	flag.StringVar(&logLevel, "log", "debug", "log level")
+	flag.IntVar(&port, "p", port, "webserver port")
+	flag.IntVar(&db, "db", db, "database (0 - Redis | 1 - Mock)")
+	flag.StringVar(&dbConfigFile, "dbConf", dbConfigFile, "config file path")
+	flag.StringVar(&logLevel, "log", logLevel, "log level")
 
 	// Parse arguments
 	flag.Parse()
@@ -34,12 +33,19 @@ func main() {
 		fmt.Println(err)
 	}
 
-	// New Controller
-	taskCtrl := web.NewTaskController(taskDAO)
+	// New webserver
+	webServer := negroni.New()
 
-	// New Router
-	router := web.NewRouter(taskCtrl)
-	
-	fmt.Println("Starting web server on port : " + strconv.Itoa(port))
-	http.ListenAndServe(":"+strconv.Itoa(port), router)
+	recovery := negroni.NewRecovery()
+	recovery.PrintStack = false
+	webServer.Use(recovery)
+
+	// New controller
+	taskController := web.NewTaskController(taskDAO)
+
+	// New router
+	router := web.NewRouter(taskController)
+
+	webServer.UseHandler(router)
+	webServer.Run(":" + strconv.Itoa(port))
 }
